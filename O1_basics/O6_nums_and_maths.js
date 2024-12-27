5b51b3fa098f05a38fcdbf8a039f6b5b8df889d4
Prateek Agarwal <127413279+agprateek7@users.noreply.github.com>
2024-12-27 08:42:24 +0000

    nums and maths done

---
+++ O1_basics/O6_nums_and_maths.js
@@ -0,0 +1,45 @@
+const score = 400
+
+const balance = new Number(100)
+
+// console.log(typeof(balance))
+
+// console.log(typeof(score))
+
+// console.log(balance)
+
+// console.log(balance.toString().length)
+
+// console.log(balance.toFixed(2))
+
+const otherNumber = 23.263
+
+// console.log(otherNumber.toPrecision(3))
+// console.log(otherNumber.toPrecision(4))
+// console.log(otherNumber.toFixed(2))
+
+const hundreds = 10000000
+//console.log(hundreds.toLocaleString('en-IN'))
+
+//console.log(Number.MAX_VALUE)
+
+// console.log(Math)
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(-4.3))
+
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.2))
+
+// console.log(Math.max(4, 3, 6, 8))
+// console.log(Math.min(4, 3, 6, 8))
+
+//console.log(Math.ceil(Math.random() * 10))
+
+//console.log((Math.random() * 10) + 1)
+
+const min = 10
+const max = 20
+
+//console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
