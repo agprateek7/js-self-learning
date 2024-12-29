c6c0626429396db87e92100a00100a5b4e98d1e2
Prateek Agarwal <127413279+agprateek7@users.noreply.github.com>
2024-12-29 13:15:16 +0000

    functions continue

---
+++ O3_basics/O1_functions.js
@@ -0,0 +1,23 @@
+function sayMyName (){
+    console.log("Prateek")
+}
+
+//sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2)
+// }
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+//addTwoNumbers(2, 4)
+//addTwoNumbers(3, "a")
+//addTwoNumbers(3, null)
+
+const sum = addTwoNumbers(3, 5)
+
+console.log("Result: ", sum)
