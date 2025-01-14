4315071b8fe66359b3f9fa8db9c5cd2fd9a6b472
Prateek Agarwal <127413279+agprateek7@users.noreply.github.com>
2025-01-14 12:51:42 +0000

    iife done

---
+++ O3_basics/O4_iife.js
@@ -0,0 +1,14 @@
+// iife - Immediately Invoked Function Exprassion
+
+// (function chai() {
+//     console.log(`Db Connected`)
+// })();
+
+(() => {
+    console.log(`Db Connected 2`)
+})();
+//chai()
+
+((name) => {
+    console.log(`this name is ${name}`)
+})("Typhon");
