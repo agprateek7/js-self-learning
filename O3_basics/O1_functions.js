5b0f6290bf1c707b46c5e8e0f4bbbd3500be4f9a
Prateek Agarwal <127413279+agprateek7@users.noreply.github.com>
2025-01-14 11:13:13 +0000

    scope continue

--- O3_basics/O1_functions.js
+++ O3_basics/O1_functions.js
@@ -33,5 +33,35 @@ function loginUserMessage(username){
 // }
 
 //console.log(loginUserMessage("Prateek"))
-console.log(loginUserMessage())
+//console.log(loginUserMessage())
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+//console.log(calculateCartPrice(200, 400, 600))
+
+const user = {
+    username: "prateek",
+    price: 19999999999
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+//handleObject(user)
+
+// handleObject({
+//     username: "sam",
+//     price: 399
+// })
+
+const myNewArray = [200, 100, 400, 600]
+
+function findSecondValue(getArray){
+    return getArray[1]
+}
+
+//console.log(findSecondValue(myNewArray))
 
---
+++ O3_basics/O2_scopes.js
@@ -0,0 +1,19 @@
+let a = 100
+// const b = 20
+// var c = 30
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+    console.log(a)
+}
+
+//console.log(c)
+console.log(a)
+//console.log(b)
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
